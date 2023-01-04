f307c4e8e94b0d868f58a1baa62af0533e3d66bb
HYUNJEE <hyunjeesong@gmail.com>
2023-01-05 02:46:47 +0900

    JavaScript - display items

--- Javascript 쇼핑몰 미니게임 과제/main.js
+++ Javascript 쇼핑몰 미니게임 과제/main.js
@@ -7,10 +7,27 @@ function loadItems(){
 }
 
 
+function displayItems(items){
+    const container = document.querySelector('.items');
+    const html = items.map(item=>createHTMLString(item)).join('');
+    console.log(html);
+    container.innerHTML = items.map(item=>createHTMLString(item)).join('');
+
+}
+function createHTMLString(item){
+    return `
+    <li class="item">
+        <img src="${item.image}" alt="${item.type}" class="item_thumbnail" />
+        <span class="item_description">${item.gender}, ${item.size}</span>
+    </li>
+    `;
+}
+
 /// main
 loadItems() 
     .then(items => {
         console.log(items);
+        displayItems(items);
     //displayItems(items);
     //setEventListeners(items);
     })

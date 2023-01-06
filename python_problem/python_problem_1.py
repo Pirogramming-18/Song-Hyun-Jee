a8489d7b88392d1bf5d568fe1c9a8f52274e2a21
HYUNJEE <hyunjeesong@gmail.com>
2023-01-06 23:47:11 +0900

    1-3

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,2 +1,10 @@
 num = 0
-player_num = float(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
+while (True):
+    player_num = float(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
+    if player_num % 1 != 0:
+        print('정수를 입력하세요')
+    elif player_num < 1 or player_num > 3:
+        print('1,2,3 중 하나를 입력하세요')
+    else:
+        player_num = int(player_num)
+        break

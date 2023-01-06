06f95637a0a72fb7b0e5ae5dec6d0bcff19ab6b3
HYUNJEE <hyunjeesong@gmail.com>
2023-01-07 05:53:10 +0900

    input에서 .0 -> 정수로 안받음 수정

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -3,14 +3,16 @@ import random
 
 def brGame():
     while (True):
-        player_num = float(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
-        if player_num % 1 != 0:
-            print('정수를 입력하세요')
-        elif player_num < 1 or player_num > 3:
-            print('1,2,3 중 하나를 입력하세요')
-        else:
+        player_num = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :')
+        try:
             player_num = int(player_num)
-            return player_num
+            if player_num < 1 or player_num > 3:
+                print('1,2,3 중 하나를 입력하세요')
+            else:
+                player_num = int(player_num)
+                return player_num
+        except ValueError:
+            print('정수를 입력하세요')
 
 
 num = 0
